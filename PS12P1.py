0e3cc355c99287eecb40366279fe7f5e825e99ea
George01284 <g_marijan@mail.harpercollege.edu>
2024-11-12 16:16:08 -0800

    Add files via upload

---
+++ PS12P1.py
@@ -0,0 +1,9 @@
+last_Names = ["Smith", "Jones","Johnson","Kevin","Ricardo","William","Jordan","Samuel","Jackson","Richardson"]
+for name in last_Names:
+    print(name)
+
+last_Names.sort()
+
+print("\nSorted order:")
+for name in last_Names:
+    print(name)
