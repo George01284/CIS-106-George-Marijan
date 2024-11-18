02177ea486002309c2b71b4c6cf1e8f455dd326c
George01284 <g_marijan@mail.harpercollege.edu>
2024-11-17 22:12:22 -0800

    Delete PS12P1.py

--- PS12P1.py
+++
@@ -1,9 +0,0 @@
-last_Names = ["Smith", "Jones","Johnson","Kevin","Ricardo","William","Jordan","Samuel","Jackson","Richardson"]
-for name in last_Names:
-    print(name)
-
-last_Names.sort()
-
-print("\nSorted order:")
-for name in last_Names:
-    print(name)
